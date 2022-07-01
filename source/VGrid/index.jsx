41032c5834add98e078011f2d11fd7034b1e349b
fedeghe <fedeghe@gmail.com>
2022-07-01 17:06:27 +0200

    nope

--- source/VGrid/index.jsx
+++ source/VGrid/index.jsx
@@ -1,33 +1,31 @@
 import React, { useReducer } from 'react';
-import { ThemeProvider } from 'react-jss';
+
 import GridContext from './Context';
 import reducerFactory from './reducer';
 import Grid from './Grid';
 
 import useStyles from './style.js';
 
-export default ({ config }) => {
+export default ({config}) => {
     const { reducer, init } = reducerFactory(),
         // initialState = useMemo(() => init(config), [config, init]),
-        [state, dispatch] = useReducer(reducer, config, init),
+        [ state, dispatch ] = useReducer(reducer, config, init),
         {
             dimensions: {
                 width, height
             },
             Loader,
-            virtual: { loading }
+            virtual: {loading}
         } = state,
         classes = useStyles({
             width, height,
         });
-
-    return <ThemeProvider theme={{}}>
-        <div className={[classes.Wrapper, 'Wrapper'].join(' ')}>
-            <GridContext.Provider value={{ state, dispatch }}>
-                {loading && <div className={classes.LoaderContainer}><Loader /></div>}
-                <Grid />
-            </GridContext.Provider>
-        </div>
-    </ThemeProvider>;
+    
+    return <div className={[classes.Wrapper, 'Wrapper'].join(' ')}>
+        <GridContext.Provider value={{state, dispatch}}>
+            {loading && <div className={classes.LoaderContainer}><Loader/></div>}
+            <Grid/>
+        </GridContext.Provider>
+    </div>;
 };
 
